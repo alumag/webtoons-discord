98b06ab7c2829093b7cc591174730f2117295f15
Aluma Gelbard <violetaluma@hotmail.com>
2019-04-05 16:32:34 +0300

    Webhook notification for DailyRelease updates

---
+++ notification.py
@@ -0,0 +1,75 @@
+from textwrap import wrap
+from DiscordHooks import Hook, Embed, EmbedAuthor, Color, EmbedField
+
+from parse_webtoons import parse_releases, genres
+
+img = "https://i.imgur.com/pFDz8Ixl.jpg"
+webhook = "SECRET WEBHOOK URL"
+
+
+class EmbedFieldInline(EmbedField):
+    """Inline version for EmbedField"""
+    __items__ = ('name', 'value', 'inline')
+
+    @property
+    def inline(self) -> bool:
+        return True
+
+
+class ReleaseHook(object):
+
+    def __init__(self):
+        self.cards = parse_releases()
+
+    def genre_sort(self):
+        """Sort the cards by genre"""
+        ret = dict()
+        for genre in genres:
+            ret[genre] = [card for card in self.cards
+                          if card.genre == genre]
+        return ret
+
+    @staticmethod
+    def _text(cards):
+        text = ""
+        for card in cards:
+            name = "\n".join(wrap(card.subject, 28))
+            text += "[{}]({})\n".format(name, card.href)
+        return text if text != "" else "None for today!"
+
+    def add_fields(self, embed):
+        organized = self.genre_sort()
+
+        # Add labeled genres
+        labels = ["Slice of life", "Drama", "Action",
+                  "Comedy", "Romance", "Fantasy"]
+        emoji = "🌿🎭🎬😂💞✨"
+        for genre, emo in zip(labels, emoji):
+            field = EmbedFieldInline(name=emo + " " + genre,
+                                     value=self._text(organized.pop(genre)))
+            embed.fields.append(field)
+
+        # Add "Other" category
+        other = list()
+        for cards in organized.values():
+            other.extend(cards)
+
+        field = EmbedField(name= "⁉ Other", value=self._text(other))
+        embed.fields.append(field)
+
+    def send(self):
+        """Make embed message with all the release and send them with hooks"""
+        embed = Embed(description="Here are the Webtoons updates for today!",
+                      color=Color.Lime,
+                      author=EmbedAuthor(name="New Updates!", icon_url=img),
+                      title="")
+        self.add_fields(embed)
+
+        # Send
+        hook = Hook(
+            hook_url=webhook,
+            username="Webtoons Update",
+            avatar_url=img,
+            embeds=[embed]
+        )
+        hook.execute()
